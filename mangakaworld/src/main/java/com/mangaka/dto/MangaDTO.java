528bcd651c2eeb52268e4530442c1db440ee204f
GOGO <georgi.yazovaliiski@gmail.com>
2024-03-22 11:05:25 +0200

    getters and setters for manga dto

--- mangakaworld/src/main/java/com/mangaka/dto/MangaDTO.java
+++ mangakaworld/src/main/java/com/mangaka/dto/MangaDTO.java
@@ -7,4 +7,37 @@ public class MangaDTO {
     private String name;
     private String description;
     private List<String> pagesFileNames;
+
+    public String getMangaka() {
+        return this.mangaka;
+    }
+
+    public void setMangaka(String mangaka) {
+        this.mangaka = mangaka;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<String> getPagesFileNames() {
+        return this.pagesFileNames;
+    }
+
+    public void setPagesFileNames(List<String> pagesFileNames) {
+        this.pagesFileNames = pagesFileNames;
+    }
+
 }
